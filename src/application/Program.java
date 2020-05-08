03a8e1aa484e88a66f221f3e2e0b20db36b90b77
Andre Zaniboni <afzaniboni@gmail.com>
2020-05-07 22:30:13 -0300

    Mensagem Boa Noite e ajustes.

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,9 +4,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Bom dia!!");
-		System.out.println("Bom tarde!!");
-
+		System.out.println("Bom Dia!!");
+		System.out.println("Boa Tarde!!");
+		System.out.println("Boa Noite!!");
+		
 	}
 
 }
